dc7c641aceb76e02fe4a4f508b678bc75da96e2b
BloomShell <77837516+BloomShell@users.noreply.github.com>
2022-10-05 19:31:41 +0200

    Create summary.cpp

---
+++ demo/summary.cpp
@@ -0,0 +1,24 @@
+#include "../hpp/base.h"
+#include "../hpp/benchmark.h"
+
+
+void summary(int argc, char** argv) {
+
+	// Initialize the Ticker object:
+	yfinance::Symbol* tk;
+	tk = new yfinance::Symbol("GS");
+
+	// Loading assetProfile into struct:
+	auto profile = tk->get_profile();
+
+	// Print assetProfile:
+	std::cout << profile;
+
+	// Loading Module (quoteSummary):
+	auto quoteSummary = tk->get_summary("financialData");
+
+	// Print Module:
+	for (auto& [k, v] : quoteSummary.items())
+		std::cout << k << ':' << v << "\n";
+
+}
